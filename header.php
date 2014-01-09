de0b4b7216591c1c2041c5217cf03893504b0be8
Lemoine Simon <lemoine.simon.perso@gmail.com>
2014-01-09 23:41:13 +0100

    Premier commit du site
    Importation des fichiers de mathieu du google drive vers le local.
    Intégration et modification des fichiers menu.php en header.php.
    Création du fichier footer.php et index.php.

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PeakCSS - FrameWork CSS - Create your own CSS - Select, custom, generate CSS</title>
+        <link href="style.css" rel="stylesheet"/>
+        <link href='http://fonts.googleapis.com/css?family=Raleway:400,200,300,100' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+        <header>
+            <div class="content">
+                <h1>
+                    <a href="#">Peak<span>CSS</span></a>
+                </h1>
+                <nav>
+                    <ul>
+                        <li><a href="">Home</a></li> 
+                        <li><a href="">Customize</a></li>
+                        <li class="active"><a href="">Explore</a></li>
+                        <li><a href="">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
