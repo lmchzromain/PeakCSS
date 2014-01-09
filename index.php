accfdb770f484408ed84c803a66df16807369cab
Romain <lemichez.romain@outlook.com>
2014-01-09 23:12:48 +0100

    Modif index.php

--- index.php
+++ index.php
@@ -10,30 +10,28 @@
         <meta name="viewport" content="width=device-width">
     </head>
     <body>
-        <div class="content">
-            <div id="HowUse">
-                <h2>How to use ?</h2>
-                <div id="select">
-                    <div class="text left">
-                        <h3>1. Select</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, ea, totam veritatis harum dolore cupiditate aut ad libero voluptate nam praesentium voluptatibus nulla tempora necessitatibus similique suscipit ratione rem recusandae.</p>
-                    </div>
-                    <div class="pic right"></div>
+        <div id="HowUse">
+            <h2>How to use ?</h2>
+            <div id="select">
+                <div class="text left">
+                    <h3>1. Select</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, ea, totam veritatis harum dolore cupiditate aut ad libero voluptate nam praesentium voluptatibus nulla tempora necessitatibus similique suscipit ratione rem recusandae.</p>
                 </div>
-                <div id="custom">
-                    <div class="pic left"></div>
-                    <div class="text right">
-                        <h3>2. Custom</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, iusto, accusamus perspiciatis facere mollitia aliquam cumque similique ratione? Fuga, doloribus dolore voluptates sint cum ex aliquam voluptatum? Maxime, fugit, doloremque.</p>
-                    </div>    
+                <div class="pic right"></div>
+            </div>
+            <div id="custom">
+                <div class="pic left"></div>
+                <div class="text right">
+                    <h3>2. Custom</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, iusto, accusamus perspiciatis facere mollitia aliquam cumque similique ratione? Fuga, doloribus dolore voluptates sint cum ex aliquam voluptatum? Maxime, fugit, doloremque.</p>
+                </div>    
+            </div>
+            <div id="generate">
+                <div class="text left">
+                    <h3>3. Generate</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, officia, doloribus, dolore perspiciatis et illo earum quos sapiente autem numquam necessitatibus cum quidem repudiandae vitae nesciunt culpa dolores alias omnis!</p>
                 </div>
-                <div id="generate">
-                    <div class="text left">
-                        <h3>3. Generate</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, officia, doloribus, dolore perspiciatis et illo earum quos sapiente autem numquam necessitatibus cum quidem repudiandae vitae nesciunt culpa dolores alias omnis!</p>
-                    </div>
-                    <div class="pic right"></div>         
-                </div>
+                <div class="pic right"></div>         
             </div>
         </div>
     </body>
