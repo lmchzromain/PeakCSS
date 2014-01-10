39e1fdb39c763206c31e4fc715f974a9cd9c427d
Lemoine Simon <lemoine.simon.perso@gmail.com>
2014-01-10 22:21:26 +0100

    Modification de la page customize
    Modification de customize.php style.css, header, colpick.css et .js

--- customize.php
+++ customize.php
@@ -1,33 +1,96 @@
+<?php REQUIRE 'header.php'; ?>
+<div id="choice_box">
+    <div class="choice">
+        <form>
+            <input type="checkbox" name="text" value="Text"> Text </input>
+            <input type="checkbox" name="button" value="Button"> Button </input>
+			<input type="checkbox" name="input" value="Input"> Input </input>
+			<input type="checkbox" name="block" value="Block"> Block </input>
+			<input type="checkbox" name="table" value="Table"> Table </input>
+        </form>
+    </div>
+</div>
+<section>
+    <div class="toggle_text">
+        <a href="#" class="toggler"><!--<img class="fleche" src="img/fleche_bas.png" alt="fleche" width="15px">-->Text</a>
+    </div>	
+    <div class="toggle">
+        <table>
+            <tr>
+                <th></th>
+                <th>FONT</th>
+                <th>SIZE</th>
+                <th>COLOR</th>
+                <th>SHADOW</th>
+            </tr>
+            <tr>
+                <td><input type="text" class="radius-all" value="Example"></td>
+                <td>
+                    <select>
+                        <option>Arial</option>
+                        <option>Helvetica</option>
+                        <option>Cambria</option>
+                        <option>Times New Roman</option>
+                    </select>
+                </td>
+                <td>
+                    <select>
+<?php 
+    for($i=10; $i<41; $i++){
+        echo '<option value="'.$i.'">'.$i.' px</option>';
+        $i++;
+    }
+?>
+                    </select>
+                </td>
+                <td><span class="before-input radius-left">#</span><input type="text" class="picker radius-right"></input></td>
+                <td><span class="before-input radius-left">H</span><input type="text" class="shadow radius-right"/>
+                    <span class="before-input radius-left">V</span><input type="text" class="shadow radius-right"/>
+                    <span class="before-input radius-left">B</span><input type="text" class="shadow radius-right"/>
+                    <span class="before-input radius-left">D</span><input type="text" class="shadow radius-right"/></td>
+            </tr>
+        </table>
+    </div>
 
-			<?php REQUIRE 'header.php'; ?>
-
-			<div id="choice_box">
-				<div class="choice">
-					<form>
-						<input type="checkbox" name="text" value="Text"> Text </input>
-						<input type="checkbox" name="button" value="Button"> Button </input>
-						<input type="checkbox" name="input" value="Input"> Input </input>
-						<input type="checkbox" name="block" value="Block"> Block </input>
-						<input type="checkbox" name="table" value="Table"> Table </input>
-					</form>
-				</div>
-			</div>
-
-			<div id="toggle_choice">
-
-				<div id="toggle_text">
-					<a href="#" id="toggler"> <img class="fleche" src="img/fleche_bas.png" alt="fleche" width="15px"> Texte </a>
-				</div>	
-				<div id="toggle" style="display:none;">
-					Toggle element 
-				</div>
-
-				<div id="toggle_text">
-					<a href="#" id="toggler"> <img class="fleche" src="img/fleche_bas.png" alt="fleche" width="15px"> Button </a>
-				</div>	
-				<div id="toggle" style="display:none;">
-					Toggle element 
-				</div>
-			</div>
-
-			<?php require 'footer.php'; ?>
+    <div class="toggle_text">
+        <a href="#" class="toggler"><!--<img class="fleche" src="img/fleche_bas.png" alt="fleche" width="15px">-->Button</a>
+    </div>	
+    <div class="toggle">
+        <table>
+            <tr>
+                <th></th>
+                <th>FONT</th>
+                <th>SIZE</th>
+                <th>COLOR</th>
+                <th>SHADOW</th>
+            </tr>
+            <tr>
+                <td><input type="button" class="" value="Example"></td>
+                <td>
+                    <select>
+                        <option>Arial</option>
+                        <option>Helvetica</option>
+                        <option>Cambria</option>
+                        <option>Times New Roman</option>
+                    </select>
+                </td>
+                <td>
+                    <select>
+<?php 
+    for($i=10; $i<41; $i++){
+        echo '<option value="'.$i.'">'.$i.' px</option>';
+        $i++;
+    }
+?>
+                    </select>
+                </td>
+                <td><span class="before-input radius-left">#</span><input type="text" class="picker radius-right"></input></td>
+                <td><span class="before-input radius-left">H</span><input type="text" class="shadow radius-right"/>
+                    <span class="before-input radius-left">V</span><input type="text" class="shadow radius-right"/>
+                    <span class="before-input radius-left">B</span><input type="text" class="shadow radius-right"/>
+                    <span class="before-input radius-left">D</span><input type="text" class="shadow radius-right"/></td>
+            </tr>
+        </table>
+    </div>
+</section>
+<?php require 'footer.php'; ?>
--- header.php
+++ header.php
@@ -1,26 +1,37 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <link type="text/css" rel="stylesheet" href="css/reset.css"/>
         <link type="text/css" rel="stylesheet" href="css/style.css"/>
+        <link type="text/css" rel="stylesheet" href="css/colpick.css"/>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>◭ PeakCSS - Home</title>
+        <title>PeakCSS - Home</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width">
 
-        <script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'></script>
-            <script type='text/javascript'>
-                // On attend que la page soit chargée 
-                $(document).ready(function(){
-                   // On cache la zone de texte
-                   $('#toggle').hide();
-                   // toggle() lorsque le lien avec l'ID #toggler est cliqué
-                   $('a#toggler').click(function()
-                  {
-                      $('#toggle').toggle(400);
-                      return false;
-                   });
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+        <script type='text/javascript' src="js/colpick.js"></script>
+        <script type='text/javascript'>
+            // On attend que la page soit chargée 
+            $(document).ready(function(){
+            // On cache la zone de texte
+            $('.toggle').hide();
+            // toggle() lorsque le lien avec l'ID #toggler est cliqué
+            $('a.toggler').click(function(){
+                $('.toggle').toggle(400);
+                return false;
+            });
+            $('.picker').colpick({
+	           layout:'hex',
+	           submit:0,
+	           colorScheme:'dark',
+	           onChange:function(hsb,hex,rgb,fromSetColor) {
+		          if(!fromSetColor) $('.picker').val(hex).css('border-color','#'+hex);
+	           }
+            });
+//                .keyup(function(){
+//	               $(this).colpickSetColor(this.value);
+//                });
                 });
                 
         </script>
@@ -34,7 +45,7 @@
                 <nav>
                     <ul>
                         <li><a href="">Home</a></li> 
-                        <li><a href="">Customize</a></li>
+                        <li><a href="customize.php">Customize</a></li>
                         <li class="active"><a href="">Explore</a></li>
                         <li><a href="">Contact</a></li>
                     </ul>
