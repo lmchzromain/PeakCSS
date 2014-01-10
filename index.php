7337606c3686673a6c3b1b657eab0f6259d86e37
Romain <lemichez.romain@outlook.com>
2014-01-10 22:09:11 +0100

    Modification home

--- index.php
+++ index.php
@@ -1,44 +1,46 @@
 <?php 
-    require "header.php";
+require "header.php";
 ?>
-        <div id="HowUse">
-            <div class="content">
-                <h2>How to use ?</h2>
-                <div id="select">
-                    <div class="text">
-                        <h3>1. Select</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, ea, totam veritatis harum dolore cupiditate aut ad libero voluptate nam praesentium voluptatibus nulla tempora necessitatibus similique suscipit ratione rem recusandae.</p>
-                    </div>
-                    <div class="pic right"></div>
-                </div>
-                <div id="custom">
-                    <div class="pic"></div>
-                    <div class="text right">
-                        <h3>2. Custom</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, iusto, accusamus perspiciatis facere mollitia aliquam cumque similique ratione? Fuga, doloribus dolore voluptates sint cum ex aliquam voluptatum? Maxime, fugit, doloremque.</p>
-                    </div>    
-                </div>
-                <div id="generate">
-                    <div class="text">
-                        <h3>3. Generate</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, officia, doloribus, dolore perspiciatis et illo earum quos sapiente autem numquam necessitatibus cum quidem repudiandae vitae nesciunt culpa dolores alias omnis!</p>
-                    </div>
-                    <div class="pic right"></div>         
-                </div>
+<div id="HowUse">
+    <div class="content">
+        <h2>How to use ?</h2>
+        <div id="select">
+            <div class="text">
+                <h3>1. Select</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, ea, totam veritatis harum dolore cupiditate aut ad libero voluptate nam praesentium voluptatibus nulla tempora necessitatibus similique suscipit ratione rem recusandae.</p>
             </div>
+            <div class="pic right"></div>
         </div>
-        <div id="explore">
-            <div class="content">
-                <h2>Explore his capacities</h2>
-                <h5 id="discover">Discover of numerous framework created by the community by means of PeakCSS.</h5>
-                <div id="presentation">
-                    <div class="example" id="example1"></div>
-                    <div class="example" id="example2"></div>
-                    <div class="example" id="example3"></div>
-                </div>
-                <a href="#" id="bt_explore">Explore</a>
+        <div id="custom">
+            <div class="pic"></div>
+            <div class="text right">
+                <h3>2. Custom</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, iusto, accusamus perspiciatis facere mollitia aliquam cumque similique ratione? Fuga, doloribus dolore voluptates sint cum ex aliquam voluptatum? Maxime, fugit, doloremque.</p>
+            </div>    
+        </div>
+        <div id="generate">
+            <div class="text">
+                <h3>3. Generate</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, officia, doloribus, dolore perspiciatis et illo earum quos sapiente autem numquam necessitatibus cum quidem repudiandae vitae nesciunt culpa dolores alias omnis!</p>
             </div>
+            <div class="pic right"></div>         
         </div>
+    </div>
+</div>
+<div id="explore">
+    <div class="content">
+        <h2>Explore his capacities</h2>
+        <h5 id="discover">Discover of numerous framework created by the community by means of PeakCSS.</h5>
+        <div id="presentation">
+            <div class="example" id="example1"></div>
+            <div class="example" id="example2"></div>
+            <div class="example" id="example3"></div>
+        </div>
+        <a href="#" id="bt_explore">Explore</a>
+    </div>
+</div>
+<div id="quote">  
+</div>
 <?php
-    require "footer.php";
+require "footer.php";
 ?>
