0a38a53f895ed0f3d5497f51caf679a1d7600e69
Mathieu <mathieu.dou@gmail.com>
2014-01-28 21:48:23 +0100

    MAJ Conflits 2

--- header.php
+++ header.php
@@ -94,7 +94,8 @@
         <header>
             <div class="content">
                 <h1>
-                    <a href="index.php">Peak<span>CSS</span></a>
+
+                    <a href="index.php">PEAK CSS</a>
                 </h1>
                 <nav>
                     <ul>
