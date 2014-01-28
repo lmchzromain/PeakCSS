834d4317bb3a72b98d4000083c790f412f0f885a
Mathieu <mathieu.dou@gmail.com>
2014-01-28 08:49:15 +0100

    MAJ contact, footer, CSS

--- footer.php
+++ footer.php
@@ -1,4 +1,34 @@
 <footer>
+    <div class="content">
+        
+        <div class="box1">
+            <h3 class="inset-text">
+                PeakCSS           
+            </h3>
+            <ul>
+                <li><a href="index.php"> Home </a></li>
+                <li><a href="customize.php"> Customize </a></li>
+                <li><a href="explore.php"> Explore </a></li>
+                <li><a href="contact.php"> Contact </a></li>                    
+            </ul>            
+        </div>
+        
+        <div class="box_center">
+              <p>  &copy; Copyright 2014 PeakCSS </p>
+        </div>
+        
+        <div class="box2">
+             <h3>
+                About US
+            </h3>  
+            <ul>
+                <li><a href="index.php"> Team PeakCSS </a></li>
+                <li><a href="explore.php"> The project </a></li>
+                <li><a href="customize.php"> Conditions of use </a></li>
+            </ul>          
+        </div>   
+           
+    </div>
 </footer>
 <script src="./js/anim_home.js" type="text/javascript" language="JavaScript"></script>
 </body>
