31b72eb4969252b496ee460fc3d0c3e8142f73e8
Lemoine Simon <lemoine.simon.perso@gmail.com>
2014-01-30 14:44:26 +0100

    Modif project team et footer

--- project_team.php
+++ project_team.php
@@ -1,40 +1,42 @@
 <?php require 'header.php'; ?>
-         <div id="explore_2">
-            <div class="content">        
-                <div class="project_team">            
+            <div class="project_team">
+                <div class="content">
                     <h3>Team PeakCSS</h3>            
                     <div id="team">
                         <div class="member">
-                            <a href="http://simonlemoine.fr" target="_blank"> <img src="img/team/Simon_Lemoine.jpg" alt="Simon LEMOINE">  </a>    
-                            <span class="presentation">
+                            <a href="http://simonlemoine.fr" target="_blank">
+                                <img src="img/team/Simon_Lemoine.jpg" alt="Simon LEMOINE" width="100%" height="auto">
+                            </a>    
+                            <div class="presentation">
                                 <h3> Simon LEMOINE </h3></br>
-                                   <p> My name is Simon Lemoine, 20, a student in the web, currently in BS Degree of University in Valenciennes. Passionate and curious new media technologies I successfully obtained a DUT Services and Communication Networks at the IUT de Lens.</p>
-                            </span>                      
+                                   <p>I'm Simon, 20 years old, student in BS Degree of University in Valenciennes. I'm passionate and curious about new media and web technologies. I successfully obtained a DUT Services and Communication Networks at the IUT de Lens. I wanna be a Front-End Developper. I like to write splendid and smart code.</p>
+                            </div>                      
                         </div> 
                         <div class="member">
-                            <a href="http://mathieu-douhet.fr" target="_blank"> <img src="img/team/Mathieu_Douhet.jpg" alt="Simon LEMOINE">  </a>    
-                            <span class="presentation">
+                            <a href="http://mathieu-douhet.fr" target="_blank">
+                                <img src="img/team/Mathieu_Douhet.jpg" alt="Mathieu DOUHET" width="100%" height="auto">
+                            </a>    
+                            <div class="presentation">
                                 <h3> Mathieu DOUHET </h3></br>
                                   <p> My name is Mathieu, I'm 20 years old. After my DUT Services and Communication Networks, IUT de Lens. I went to a BS Degree in Information Technology and the Internet. I am passionate about the development, integration web. Curious in the field of new technologies</p>
-                            </span>                      
+                            </div>                      
                         </div>
                         <div class="member">
-                            <a href="http://romainlemichez.com" target="_blank"> <img src="img/team/Romain_Lemichez.jpg" alt="Simon LEMOINE">  </a>    
-                            <span class="presentation">
+                            <a href="http://romainlemichez.com" target="_blank">
+                                <img src="img/team/Romain_Lemichez.jpg" alt="Romain LEMICHEZ" width="100%" height="auto">
+                            </a>    
+                            <div class="presentation">
                                  <h3>Romain LEMICHEZ </h3></br>
-                                   <p>My name is Roman, passionate about the field of web and new technologies, I have the ambition to become a web                                        developer. I am currently in BS Degree in Information Technology and the Internet of University in Valenciennes                                     (FRANCE, 59)</p>
-                            </span>                      
+                                   <p>My name is Romain, passionate about the field of web and new technologies, I have the ambition to become a web developer. I am currently in BS Degree in Information Technology and the Internet of University in Valenciennes (FRANCE, 59)</p>
+                            </div>                      
                         </div>                                                  
                     </div>
                 </div>
             </div>
-         </div>
-    
-           
         <div class="content">        
-            <div id="select" class="line">
+            <div id="select">
                 <div class="description">
-                    <h3>The project & context</h3>
+                    <h3 id="project">The project & context</h3>
                     <p>We are 3 students, BS Degree in Information Technology and the Internet. This project is an open source project, So he's not                         sponsored by a business or for an professional agency. The main objective of this web site is going to be to propose a tool                           allowing to create a base CSS by simplifying the programming thanks to a simplistic interface and without touch the code CSS.</p>
                 </div>
                 <div class="pic right">
@@ -42,6 +44,4 @@
                 </div>
             </div>    
         </div>
-
-
 <?php require 'footer.php'; ?>
