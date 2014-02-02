57fba50912743ac185f2943eed8e7cd9fd033d13
Lemoine Simon <lemoine.simon.perso@gmail.com>
2014-02-02 22:41:25 +0100

    Commit du dimanche

--- customize.php
+++ customize.php
@@ -241,7 +241,7 @@ if (!empty($_POST)){
     $filenamehtml = "PeakHTML"; 
     $openhtml = fopen("download/".$filenamehtml.".html", "w");
     fwrite($openhtml, $textehtml); 
-    fclose($openhtml); 
+    fclose($openhtml);
     
     //Zippage des fichiers
     $zip = new ZipArchive(); 
@@ -257,7 +257,7 @@ if (!empty($_POST)){
     //Téléchargement
     header('Content-Type: text/plain');
     header('Content-Disposition: attachment; filename="PeakCSS.zip"');
-    readfile('download/peakCSS.zip');
+    readfile('download/PeakCSS.zip');
     unlink('download/PeakHTML.html');
     unlink('download/PeakCSS.css');
     unlink('download/PeakCSS.zip');
